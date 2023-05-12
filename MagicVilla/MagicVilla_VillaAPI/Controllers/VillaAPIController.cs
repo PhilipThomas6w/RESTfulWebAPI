7ff79e7cca713a29fec206c90cc91c67b9756222
Philip Thomas <110606536+PhilipThomas6w@users.noreply.github.com>
2023-05-12 23:05:55 +0100

    Added VillaDTO

--- MagicVilla/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,11 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MagicVilla_VillaAPI.Controllers;
 
-[ApiController] // this attribute notifies ASP.NET that this class will be an API controller
+
+[Route("api/villaAPI")]   // This is an attribute route/endpoint. Action methods on controllers annotated with ApiController attribute must be attribute routed.
+[ApiController] // the ApiController attribute notifies ASP.NET that this class will be an API controller
 public class VillaAPIController : ControllerBase
 {
     // first API endpoint
-
+    
+    [HttpGet] // identifies the action method as an HTTP method
+    public IEnumerable<VillaDTO> GetVillas()
+    {
+        return new List<VillaDTO>
+        {
+            new VillaDTO { Id = 1, Name = "Pool View" },
+            new VillaDTO { Id = 2, Name = "Beach View" }
+        };
+    }
 
 }
