0fb68f6659b4b4c277dc5a807c7786c4b87276bc
Philip Thomas <110606536+PhilipThomas6w@users.noreply.github.com>
2023-05-12 22:49:55 +0100

    Added empty controller and first model

---
+++ MagicVilla/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers;
+
+[ApiController] // this attribute notifies ASP.NET that this class will be an API controller
+public class VillaAPIController : ControllerBase
+{
+    // first API endpoint
+
+
+}
