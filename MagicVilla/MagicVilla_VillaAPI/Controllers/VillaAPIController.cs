fb9eea7b3858f54c77c03fc5d58bcb5c1a83a74f
Philip Thomas <110606536+PhilipThomas6w@users.noreply.github.com>
2023-05-13 01:30:42 +0100

    Added post and delete endpoints

--- MagicVilla/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla/MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -6,26 +6,102 @@ using Microsoft.AspNetCore.Mvc;
 namespace MagicVilla_VillaAPI.Controllers;
 
 
-[Route("api/VillaAPI")]   // This is an attribute route/endpoint. Action methods on controllers annotated with ApiController attribute must be attribute routed.
+[Route("api/VillaAPI")] // This is an attribute route/endpoint. Action methods on controllers annotated with ApiController attribute must be attribute routed.
 [ApiController] // the ApiController attribute notifies ASP.NET that this class will be an API controller
 public class VillaAPIController : ControllerBase
 {
     // first API endpoint
-    
+
     [HttpGet] // identifies the action method as an HTTP method
-    public IEnumerable<VillaDTO> GetVillas()
+    public ActionResult<IEnumerable<VillaDTO>> GetVillas()
     {
-        return VillaStore.villaList;
+        return Ok(VillaStore.villaList);
 
     }
 
 
-    [HttpGet("id")]
-    public VillaDTO GetVillas(int id)
-    {
-        // ReturnFor each u in the collection, check if its Id property is equal to the given id value."
-        return VillaStore.villaList.FirstOrDefault(u => u.Id == id);    
+    [HttpGet("{id:int}", Name = "GetVilla")]
 
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(404)]
+    [ProducesResponseType(400)]
+    //[ProducesResponseType(200, Type = typeof(VillaDTO))] // If we used this, we could just use ActionResult<> as the return type
+    public ActionResult<VillaDTO> GetVillas(int id)
+    {
+        if (id == 0)
+        {
+            return BadRequest();
+        }
+
+        var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+
+        if (villa == null)
+        {
+            return NotFound("No villa with that id exists in the datastore.");
+        }
+
+        // In the villaList stored in the VillaStore, return the first u (i.e., VillaDTO) in the collection whose Id property is equal to the given id value."
+        return Ok(villa);
+
+    }
+
+    [HttpPost]
+    public ActionResult<VillaDTO> CreateVilla([FromBody] VillaDTO villaDTO)
+    {
+        //if (!ModelState.IsValid)
+        //{
+        //    return BadRequest(ModelState);
+        //}
+        // We would only add the above if statement if we hadn't assigned the [ApiController] attribute. It's checking if the required properties have been included in the Http request. The [ApiController] attribute does this automatically.
+
+        // custom ModelState validation
+        if (VillaStore.villaList.FirstOrDefault(u => u.Name.ToLower() == villaDTO.Name.ToLower()) != null);
+        {
+            ModelState.AddModelError("", "Villa already exists in datastore!");
+        }
+
+
+        if (villaDTO == null)
+        {
+            return BadRequest(villaDTO);
+        }
+
+        if (villaDTO.Id > 0)
+        {
+            return StatusCode(StatusCodes.Status500InternalServerError);
+        }
+
+        // Below is very convoluted, because we are using a fake database i.e., the VillaStore.cs, which is just a list. Auto-increment is easy with SQL.
+        villaDTO.Id = VillaStore.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+
+        VillaStore.villaList.Add(villaDTO);
+
+        return CreatedAtRoute("GetVilla", new { id = villaDTO.Id }, villaDTO);  // Notice we are passing the name declared in [HttpGet("{id:int}", Name = "GetVilla")]
+
+    }
+
+
+    [HttpDelete("{id:int}", Name = "DeleteVilla")]
+
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public IActionResult DeleteVilla(int id)    // If we use IActionResult rather than ActionResult<>, we don't need to declare the return type
+    {
+        if (id == 0)
+        {
+            return BadRequest();
+        }
+
+        if (Villa == null)
+        {
+            return NotFound();
+        }
+
+        var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+        VillaStore.villaList.Remove(villa);
+        return NoContent();
     }
 
 }
+
